520d26eea8155c97cdddf2fbaf2b1024a8f4d44f
sunil <sunilmalladi1349@gmail.com>
2026-06-27 10:29:01 +0530

    blink with hello

---
+++ zephyr/01-blinky/src/main.c
@@ -0,0 +1,25 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+int main(void)
+{
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+        return 0;
+    }
+
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        printk("hello world\n");
+        k_msleep(1000);
+    }
+
+    return 0;
+}
